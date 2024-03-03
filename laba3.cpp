af6dd5567c5b2997378c0ad302100bfc232944bd
frezer88 <65221470+frezer88@users.noreply.github.com>
2024-03-03 22:01:42 +0500

    create function matrixTransformation and calculationSolution, nice output added

--- laba3.cpp
+++ laba3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "/opt/homebrew/Cellar/libomp/17.0.6/include/omp.h"
 
 using namespace std;
 
@@ -11,6 +12,30 @@ void printMatrix(double** matrix, double* answerMatrix) {
             cout << matrix[i][j] << " ";
         }
         cout << " = " << answerMatrix[i] << endl;
+    }
+}
+
+void matrixTransformation(double** matrix, double* answerMatrix) {
+    for (int k = 0; k < lengthMatrix - 1; k++) {
+        #pragma omp parallel for shared(matrix, answerMatrix)//распараллелить цикл, используя общие данные matrix и answerMatrix, которые могут быть доступны для чтения и записи из всех потоков, участвующих в этой параллельной области
+        for (int i = k + 1; i < lengthMatrix; i++) {
+            double temp = matrix[i][k] / matrix[k][k];
+            for (int j = k; j < lengthMatrix; j++) {
+                matrix[i][j] -= temp * matrix[k][j];
+            }
+            answerMatrix[i] -= temp * answerMatrix[k];
+        }
+    }
+}
+
+void calculationSolution(double** matrix, double* answerMatrix, double* solution) {
+    solution[lengthMatrix - 1] = answerMatrix[lengthMatrix - 1] / matrix[lengthMatrix - 1][lengthMatrix - 1];
+    for (int i = lengthMatrix - 2; i >= 0; i--) {
+        double sum = answerMatrix[i];
+        for (int j = i + 1; j < lengthMatrix; j++) {
+            sum -= matrix[i][j] * solution[j];
+        }
+        solution[i] = sum / matrix[i][i];
     }
 }
 
@@ -39,7 +64,22 @@ int main() {
 
     double* answerMatrix = createMatrixFirst(lengthMatrix);
 
+    double* solution = new double [3];
+
+    cout << "Исходная система уравнений:" << endl;
     printMatrix(matrix, answerMatrix);
+
+    matrixTransformation(matrix, answerMatrix);
+
+    cout << "\nМатрица после прямого хода:" << endl;
+    printMatrix(matrix, answerMatrix);
+
+    calculationSolution(matrix, answerMatrix, solution);
+
+    cout << "\nРешение СЛАУ:" << endl;
+    for (int i = 0; i < lengthMatrix; i++) {
+        cout << "x" << i + 1 << " = " << solution[i] << endl;
+    }
 
     return 0;
 }
